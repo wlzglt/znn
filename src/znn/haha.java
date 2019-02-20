f503fa190cb7af67e65fe382c0bd0145e1b40be9
wanglizhu <693198602@qq.com>
2019-02-20 15:40:51 +0800

    haha01

---
+++ src/znn/haha.java
@@ -0,0 +1,11 @@
+package znn;
+/**
+* @author wanglizhu
+* @version 创建时间：2019年2月20日 下午3:40:17
+* 类说明
+*/
+public class haha {
+	public static void main(String[] args) {
+		System.out.println("01");
+	}
+}
